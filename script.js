8effc7a6296703e894bfb7cb510f6e267110f18c
Usman1487 <usmank148752@gmail.com>
2025-08-05 08:10:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nav-cont');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
